ae47036a867666b8109608ce70c06eb2ce80b203
Paul Meier <pablo.a.meier@gmail.com>
2010-06-28 10:20:16 -0400

    More rigorous testing for the wordsearch.
    We can test more aggresively!!!!11!1!one

--- test/wordsearch_test.erl
+++ test/wordsearch_test.erl
@@ -8,6 +8,12 @@
 get_fun() ->
 	make_word_function(parse(?TEST_FILE)).
 
+get_compare_list() ->
+	{"CA*", ["ACE","ACT","ARC","CAB","CAD","CAM","CAN","CAP","CAR","CAT","CAW","CAY","LAC","MAC",
+	 		 "OCA","PAC","SAC","VAC","AA","AB","AD","AE","AG","AH","AI","AL","AM","AN",
+             "AR","AS","AT","AW","AX","AY","BA","FA","HA","KA","LA","MA","NA","PA",
+			 "TA","YA","ZA"]}.
+
 simple_test() ->
 	Search = get_fun(),
 	Words = Search("dog"),
@@ -19,4 +25,11 @@ wildcard_test() ->
 	Words = Search("A*"),
 	io:format("~p~n", [Words]),
 	?assert(lists:member("AS", Words)),
-	?assert(lists:member("HA", Words)).
+	?assert(lists:member("HA", Words)),
+	
+	run_pattern_on_list(get_compare_list(), Search).
+
+
+run_pattern_on_list({Pattern, List}, Search) ->
+	MoreWords = Search(Pattern),
+	lists:map(fun (X) -> ?assert(lists:member(X, List)) end, List).
