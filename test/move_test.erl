c63e1fe90a51d91e048a6a5e9a60b2b305273dd7
Paul Meier <pablo.a.meier@gmail.com>
2010-09-20 09:30:58 -0700

    Another, trivial test case.

--- test/move_test.erl
+++ test/move_test.erl
@@ -18,13 +18,22 @@ candidate_location1_test() ->
 					?assert(contains_tile(X, Lst))
 					end, [{6,7},{11,7},{7,6},{7,8},{8,6},{8,8},{9,6},{9,8},{10,6},{10,8}]).
 
+candidate_location2_test() ->
+	Board = place_word("POPPYCOCK", right, {6,2}, new_board()),
+	Lst = generate_move_candidate_locations(Board),
+	lists:foreach(fun (X) ->
+					?assert(contains_tile(X, Lst))
+					end, [{6,1},{6,11},{7,2},{7,3},{7,4},{7,5},{7,8},{7,9},{7,10},
+						{5,2},{5,3},{5,4},{5,5},{5,8},{5,9},{5,10}]).
 
 
 %% Test whether or not the list contains the parametrized tile.
 contains_tile({Row, Col}, Lst) ->
-	io:format("Attempting with ~p,~p~n", [Row,Col]),
-	lists:any(fun (X) -> 
+	Return = lists:any(fun (X) -> 
 				{Row2, Col2} = get_tile_location(X),
-				io:format("   Matching with ~p, ~p...~n", [Row2,Col2]),
 				Row =:= Row2 andalso Col =:= Col2
-			end, Lst).
+			end, Lst),
+	case Return of 
+		true -> true;
+		_False -> io:format("~p,~p Were not present. ~n", [Row,Col])
+	end.
