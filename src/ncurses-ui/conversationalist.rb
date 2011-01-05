25cb11c3a4fb3c523605f670414d8ae890d3a2e6
Paul Meier <pablo.a.meier@gmail.com>
2011-01-04 20:51:17 -0800

    Added Ruby Thrift files, included paths in source.
    We need the Ruby lib/ files of Thrift to use the generated files,
    so they're included in lib/, just like the Erlang ones.  We also
    include all the files in the require path of conversationalist.rb,
    who is most like to use them in the current client.
    
    By the way, the current client is a pile of dung.  It may not be
    worth salvaging.

--- src/ncurses-ui/conversationalist.rb
+++ src/ncurses-ui/conversationalist.rb
@@ -18,6 +18,10 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
+
+$:.push('build/gen-rb')
+$:.unshift('lib/thrift-rb')
+
 require 'serialization.rb'
 
 
